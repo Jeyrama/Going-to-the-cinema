b2adab959655a14b5ccbdd704a3e3bb39856d8af
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-27 21:27:03 -0700

    Update Going-to-the-cinema.js

--- Going-to-the-cinema.js
+++ Going-to-the-cinema.js
@@ -26,3 +26,13 @@ More examples:
 
 
 // Solution
+
+function movie(card, ticket, perc) {
+  var costA = n = 0,
+      costB = card;
+  while (Math.ceil(costB) >= costA) {
+      costA += ticket;
+      costB += ticket * Math.pow(perc,++n);
+  }
+  return n;
+};
